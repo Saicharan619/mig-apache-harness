1dc6025f411cda8cd85563bec3821f6609554115
saicharan <141946357+Saicharan619@users.noreply.github.com>
2025-05-16 17:55:59 +0530

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt update
-apt install -y apache2
-echo "<h1>Hello from MIG with Apache! 🚀</h1>" > /var/www/html/index.html
-systemctl enable apache2
-systemctl start apache2
+yum update -y
+yum install -y httpd
+echo "<h1>Hello from MIG with Apache  🚀</h1>" > /var/www/html/index.html
+systemctl enable httpd
+systemctl start httpd
