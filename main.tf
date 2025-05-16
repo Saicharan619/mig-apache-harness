938ba0a16679dda24abc42f52e6e7cd0e8bffee9
saicharan <141946357+Saicharan619@users.noreply.github.com>
2025-05-16 17:53:46 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,52 @@
+provider "google" {
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_instance_template" "default" {
+  name_prefix = "mig-template-"
+  region      = var.region
+
+  properties {
+    machine_type = "e2-micro"
+
+    disks {
+      auto_delete  = true
+      boot         = true
+      source_image = "debian-cloud/debian-11"
+    }
+
+    network_interfaces {
+      network = "default"
+      access_config {}
+    }
+
+    metadata_startup_script = file("startup-script.sh")
+
+    tags = ["http-server"]
+  }
+}
+
+resource "google_compute_instance_group_manager" "default" {
+  name               = "apache-mig"
+  base_instance_name = "apache-instance"
+  zone               = var.zone
+  version {
+    instance_template = google_compute_instance_template.default.id
+  }
+  target_size = 1
+}
+
+resource "google_compute_firewall" "http" {
+  name    = "allow-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
+}
